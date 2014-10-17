5d03299429a63d60c6e0e891914e4e2998b9fc29
Guzz <gustavo@jardim.in>
2014-10-17 01:34:34 -0300

    3

--- js/narrativa.js
+++ js/narrativa.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 	var ev1 = Popcorn( "#hipVid0" );
   
 	ev1.code({
-	    start: 3,
-	    end: 8,
+	    start: 4,
+	    end: 18,
 	    onStart: function() {
 	      $( "#evEsq" ).css('left', '-2%');
 	    },
 	    onEnd: function() {
-	     $( "#evEsq" ).css('left', '-25%');
+	      $( "#evEsq" ).css('left', '-50%');
 	   }
 	});
 });
