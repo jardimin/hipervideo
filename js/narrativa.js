f2d1037ca133f74f3c6ca7ed825525a605860456
Jardins <contato@jardim.in>
2014-11-19 14:52:56 -0200

    Final

--- js/narrativa.js
+++ js/narrativa.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
 	var ev1 = Popcorn( "#hipVid0" );
-  
+	var comeco = 4;
+	var fim = 18;
+	var fr = 0;
+
 	ev1.code({
-	    start: 4,
-	    end: 18,
-	    onStart: function() {
-	      $( "#evEsq" ).css('left', '-2%');
-	    },
-	    onEnd: function() {
-	      $( "#evEsq" ).css('left', '-50%');
-	   }
+    start: comeco,
+    end: fim,
+    onStart: function() {
+      $( "#evEsq" ).css('left', '-2%');
+    },
+    onEnd: function() {
+      $( "#evEsq" ).css('left', '-53%');
+    }
 	});
 });
