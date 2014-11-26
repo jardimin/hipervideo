77a52165d856f665241447371d24bf3a7ace1072
Marlus Araujo <mar251@gmail.com>
2014-11-26 17:23:10 -0200

    vueapp files

---
+++ vueapp/views/home-view.vue
@@ -0,0 +1,22 @@
+<style lang="scss">
+	h1 {
+		color: blue;
+	}
+</style>
+
+
+<template>
+	<h1>Home</h1>
+	<div v-component="example">
+		conteudo dentro de um componente
+	</div>
+</template>
+
+<script>
+
+	module.exports = {
+		components: {
+			example: require('../components/example.vue')
+		}
+	}
+</script>
