a276fdae2377c5881468d3326ad5a3bd26594aa8
Guzz <gustavo@jardim.in>
2014-11-27 06:18:05 -0200

    Final da implementacao controles do video

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,12 @@
 var hipervideo = $('#hipVid0').get(0);
 var selector = $('.rangeslider').get(0);
 var seekBar = $('#seek-bar').get(0);
+var tempoCorrido = function(array) {
+  var min = array[0];
+  var sec = array[1];
+  $('#tp-cr-min').text(min);
+  $('#tp-cr-sec').text(sec);
+};
 
 var playVideo = function(argument) {
   $('body').addClass("tocando");
@@ -15,7 +21,33 @@ var seekCap = function(perc) {
   hipervideo.currentTime = timecode;
 }
 
+function toFormat(time) {
+  var secT  = Math.floor(time),
+      sec   = (secT % 60).toFixed(),
+      min   = Math.floor(secT / 60),
+      sec_  = toDigits(sec),
+      min_  = toDigits(min);
+
+  function toDigits(n) {
+    return n > 9 ? "" + n: "0" + n;
+  }
+
+  return [min_.toString(), sec_.toString()]
+}
+
 $(document).ready(function() {
+
+  hipervideo.addEventListener("loadstart" , function() {
+    var duracao = toFormat(hipervideo.duration);
+    var tempoTotal = function(array) {
+      var min = array[0];
+      var sec = array[1];
+      $('#tp-tt-min').text(min);
+      $('#tp-tt-sec').text(sec);
+    };
+
+    tempoTotal(duracao);
+  })
 
   seekBar.addEventListener("change", function() {
     // Calculate the new time
@@ -31,7 +63,9 @@ $(document).ready(function() {
   hipervideo.addEventListener("timeupdate", function() {
     // Calculate the slider value
     var value = (1000 / hipervideo.duration) * hipervideo.currentTime;
-    var fillWidth = seekBar.value / 10
+    var fillWidth = seekBar.value / 10;
+    var tempo = toFormat(hipervideo.currentTime);
+    tempoCorrido(tempo);
 
     // Update the slider value
     seekBar.value = value;
