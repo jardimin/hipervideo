7cbfb617a9e3dda17f838c93532dc43e72b9ae33
Marlus Araujo <mar251@gmail.com>
2014-12-30 20:00:06 -0200

    sidebar-chapter nao precisa de bg para animacao

--- app/vue/components/sidebar-chapter.vue
+++ app/vue/components/sidebar-chapter.vue
@@ -12,18 +12,10 @@
 			padding-right: 58px;
 			height: 28px;
 			line-height: 28px;
-		}
-		.sidebar_chapter_bg {
-			position: absolute;
-			top: 0;
-			left: 0;
 			transition: all 0.5s;
-			transform: translate3d(-100%,0,0);
-			width: 100%;
-			height: 100%;
-			background: #fff;
+			min-width: 0;
 			.sidebar.is-open & {
-				transform: translate3d(0,0,0);
+				min-width: 232px;
 			}
 		}
 	}
@@ -32,7 +24,6 @@
 <template>
 	<div class="sidebar_chapter">
 		<div class="sidebar_chapter_title">{{title}}</div>
-		<div class="sidebar_chapter_bg"></div>
 	</div>
 </template>
 
